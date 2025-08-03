d3e72e77d68fb7ecac75b7cf1a08219027d83b9d
cmong007 <ongchunming96@gmail.com>
2025-08-04 02:02:08 +0800

    update ignore and run.sh

--- run.sh
+++ run.sh
@@ -2,6 +2,16 @@
 # Exit immediately on error
 set -e
 
+#create data folder
+mkdir -p data
+
+# download db file if missing
+if [ ! -f data/gas_monitoring.db ]; then
+  curl -fSL \
+    https://techassessment.blob.core.windows.net/aiap21-assessment-data/gas_monitoring.db \
+    -o data/gas_monitoring.db
+fi
+
 # 1) Execute EDA notebook to generate cleaned_data.csv
 jupyter nbconvert --to notebook --execute eda.ipynb --inplace
 
