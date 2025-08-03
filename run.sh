4ce841ab84cf18fb778cec30749e5f4e3cf318f0
cmong007 <ongchunming96@gmail.com>
2025-08-04 01:44:40 +0800

    remove venv from run.sh

--- run.sh
+++ run.sh
@@ -2,9 +2,6 @@
 # Exit immediately on error
 set -e
 
-# Activate the virtual environment (Windows Git Bash)
-source venv/Scripts/activate
-
 # 1) Execute EDA notebook to generate cleaned_data.csv
 jupyter nbconvert --to notebook --execute eda.ipynb --inplace
 
